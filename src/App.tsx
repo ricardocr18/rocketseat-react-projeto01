87ce8a48b77eaf4ab677c41a46a456793bcb4afd
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2023-08-28 23:38:27 -0300

    criando as tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { Header } from './components/Header/header'
+import { Task } from './components/Tasks/task'
 
 function App() {
   return (
     <>
       <Header />
-      <taskList />
+      <Task/>
     </>
   )
 }
--- src/components/Tasks/task.tsx
+++ src/components/Tasks/task.tsx
@@ -1,7 +1,37 @@
-import styles from './task.module.css'
+import styles from "./task.module.css";
+import plus from "../../assets/plus.png";
+import { useState } from "react";
 
-export function Task () {
-    return {
-        
-    }
+export function Task() {
+  const [newTask, setNewTask] = useState("");
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Impede o comportamento padrão de envio do formulário
+    console.log("Nova tarefa:", newTask); // Você pode fazer algo com o valor aqui
+
+    // Limpa o campo de entrada após a submissão
+    setNewTask("");
+  };
+
+  return (
+    <section className={styles.section}>
+      <header className={styles.header}>
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Adicione uma nova tarefa"
+              onChange={(evento) => setNewTask(evento.target.value)}
+              value={newTask}
+            />
+
+            <button type="submit">
+              <span>Criar </span>
+              <img src={plus} alt="sinal de mais" />
+            </button>
+          </div>
+        </form>
+      </header>
+    </section>
+  );
 }
