94d50d4039dd2be6a6497c7e368733e261727276
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2023-08-30 11:09:44 -0300

    como deletar tasks

--- src/components/Tasks/task.tsx
+++ src/components/Tasks/task.tsx
@@ -3,6 +3,7 @@ import plus from "../../assets/plus.png";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { ITask } from "../../interfaces/iTask";
+import trash from "../../assets/trash.png"
 
 export function Task() {
   const [tasks, setTasks] = useState<ITask[]>([]);
@@ -17,20 +18,38 @@ export function Task() {
     event: React.FormEvent<HTMLFormElement>
   ) => {
     event.preventDefault(); // Impede o comportamento padrão de envio do formulário
-    //console.log("Nova tarefa:", newTask); // Você pode fazer algo com o valor aqui
-    if(newTask){
-        const newTasks = {
-            id: uuidv4(),
-            title: newTask,
-            isComplete: false,
-        }
+    if (newTask) {
+      const newTasks = {
+        id: uuidv4(),
+        title: newTask,
+        isComplete: false,
+      };
 
-        setTasks((prevTasks) => [...prevTasks,newTasks])        
-        // Limpa o campo de entrada após a submissão
-        setNewTask("");
+      setTasks((prevTasks) => [...prevTasks, newTasks]);
+      // Limpa o campo de entrada após a submissão
+      setNewTask("");
     }
-    
   };
+
+  const handleCompletedTask = (taskId: string) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, isComplete: !task.isComplete } : task
+      )
+    );
+    console.log("Total de isComplete: ", tasks);
+  };
+  
+  const compited = tasks.filter((task) => {
+    const cont = task.isComplete === true;
+    return cont;
+  }).length;
+
+ 
+  const sumTasks = tasks.length;
+  console.log("Total de tarefas: ", sumTasks);
+
+  const concluidas = ` ${compited} de ${sumTasks}`;
 
   return (
     <section className={styles.section}>
@@ -56,13 +75,29 @@ export function Task() {
         <header className={styles.headerTask}>
           <div className={styles.taskCriada}>
             <span>Tarefas criadas</span>
-            {/* <input type="text" onChange={(e) => {}} value={totalTasks} /> */}
+            <p>{sumTasks}</p>
           </div>
           <div className={styles.taskConlcuidas}>
             <span>Concluídas</span>
-            {/* <input onChange={(e) => {}} type="text" value={concluidas} /> */}
+            <p>{concluidas}</p>
           </div>
         </header>
+        <div>
+          <ul className={styles.taskList}>
+            {tasks.map((item) => (
+                <li key={item.id}>
+                    <input
+                     type="checkbox"
+                     checked={item.isComplete}
+                     onChange={() => handleCompletedTask(item.id)}
+                      />
+                    {item.title}
+                    <img src={trash} alt="Lixeira"/>
+                </li>
+              
+            ))}
+          </ul>
+        </div>
       </main>
     </section>
   );
